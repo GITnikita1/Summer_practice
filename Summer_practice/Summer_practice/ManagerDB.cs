8198e2876d06597409b740bd1429cb3f047c892c
GITnikita1 <151996022+GITnikita1@users.noreply.github.com>
2024-07-15 19:12:37 +0300

    Add files via upload

---
+++ Summer_practice/Summer_practice/ManagerDB.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Npgsql;
+using System.Data;
+
+namespace DDPlanet
+{
+    internal class ManagerDB
+    {
+        private readonly NpgsqlConnection _link;
+        public ManagerDB(string database_login)
+        {
+            _link = new NpgsqlConnection(database_login);
+        }
+        public NpgsqlConnection GetLink()
+        {
+            if (_link.State != ConnectionState.Open)
+            {
+                _link.Open();
+            }
+            return _link;
+        }
+        public void CloseConnection()
+        {
+            if (_link.State == ConnectionState.Open)
+            {
+                _link.Close();
+            }
+        }
+        public static void FirstTask(NpgsqlConnection link)
+        {
+            var code_sql = "SELECT \"Users\".user_name, \"Roles\".rol_name FROM \"Users\" JOIN \"Users_Roles\" ON \"Users\".user_id = \"Users_Roles\".user_id JOIN \"Roles\" ON \"Users_Roles\".rol_id = \"Roles\".rol_id";
+            var users_roles = link.Query(code_sql);
+            Console.WriteLine("    Пользователь                 Роль");
+            Console.WriteLine("---------------------------------------------");
+            foreach (var user_rol in users_roles)
+            {
+                Console.WriteLine($"{user_rol.user_name}\t {user_rol.rol_name}");
+            }
+        }
+        public static void SecondTask(NpgsqlConnection link)
+        {
+            var code_sql = "SELECT \"Roles\".rol_name, COUNT(\"Users_Roles\".user_id) AS user_count FROM \"Roles\" LEFT JOIN \"Users_Roles\" ON \"Roles\".rol_id = \"Users_Roles\".rol_id GROUP BY \"Roles\".rol_name";
+            var counts_rol = link.Query(code_sql);
+            foreach (var count_rol in counts_rol)
+            {
+                if (count_rol.user_count == 1)
+                {
+                    Console.WriteLine($"Роль \"{count_rol.rol_name}\" имеет {count_rol.user_count} человек.");
+                }
+                else
+                {
+                    Console.WriteLine($"Роль \"{count_rol.rol_name}\" имеют {count_rol.user_count} человек.");
+                }
+            }
+        }
+    }
+}
---
+++ Summer_practice/Summer_practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+using System.Diagnostics;
+using System.IO;
+using DDPlanet;
+
+namespace DDPlanet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DD_Planet summer practice";
+            string database_login = "Host=localhost;Port=5432;Username=postgres;Password=pg123;Database=DD_Planet";
+            ManagerDB db = new ManagerDB(database_login);
+
+            using (var link = db.GetLink())
+            {
+                Console.WriteLine("СПИСОК ЗАДАЧ");
+                Console.WriteLine("1. Вывести всех пользователей и роли, в которых они состоят.");
+                Console.WriteLine("2. По каждой роли определить количество пользователей, которые входят в эту роль.");
+                Console.WriteLine("3. Завершить работу.");
+
+                var flag = true;
+                while (flag)
+                {
+                    Console.Write("\nВыберите задачу для решения: ");
+                    var button = Console.ReadLine();
+                    if (button == "1")
+                    {
+                        ManagerDB.FirstTask(link);
+                    }
+                    else if (button == "2")
+                    {
+                        ManagerDB.SecondTask(link);
+                    }
+                    else if (button == "3")
+                    {
+                        Console.WriteLine("Вы завершили программу.");
+                        flag = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ОШИБКА!!! Выбирать можно лишь из \"СПИОК ЗАДАЧ\".");
+                        Console.WriteLine("Попробуй снова.");
+                    }
+                }
+            }
+        }
+    }
+}
